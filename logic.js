ef89eb36f11c1e4a7c528b006d5f07bb0668e3ad
Ramashree Valunj <117629029+RSSH7275@users.noreply.github.com>
2024-03-19 20:27:56 +0530

    Add files via upload

---
+++ logic.js
@@ -0,0 +1,15 @@
+// const userCard = document.getElementById("userCard");
+
+// // console.log(userCard);
+
+// let teamUsers = 5;
+
+// function taskApp() {
+//   for (let i = 0; i < teamUsers; i++) {
+//     userCard.innerHTML += `
+    
+//     `;
+//   }
+// }
+
+// taskApp();
